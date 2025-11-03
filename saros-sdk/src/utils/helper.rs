37a340d18bddf02a3d224e625b27b27ed0820754
Endale98 <daida@coin98.finance>
2025-11-03 14:04:18 +0700

    refactor: exact bin array input

--- saros-sdk/src/utils/helper.rs
+++ saros-sdk/src/utils/helper.rs
@@ -1,17 +1,25 @@
-use solana_sdk::pubkey::Pubkey;
-use rewarder_hook::rewarder_hook;
 use crate::state::{bin::BIN_ARRAY_SIZE, position::Position};
+use rewarder_hook::rewarder_hook;
+use solana_sdk::pubkey::Pubkey;
 
 pub fn find_event_authority(program_id: Pubkey) -> Pubkey {
     Pubkey::find_program_address(&[b"__event_authority"], &program_id).0
 }
 
-pub fn get_bin_array_lower(
+pub fn get_swap_pair_bin_array(
     bin_array_index: u32,
     pair: &Pubkey,
     program_id: &Pubkey,
-) -> (Pubkey, u8) {
-    let (bin_array_lower_pubkey, bump) = Pubkey::find_program_address(
+) -> (Pubkey, Pubkey, Pubkey) {
+    let (bin_array_lower_pubkey, _) = Pubkey::find_program_address(
+        &[
+            b"bin_array".as_ref(),
+            pair.as_ref(),
+            (bin_array_index - 1).to_le_bytes().as_ref(),
+        ],
+        program_id,
+    );
+    let (bin_array_middle_pubkey, _) = Pubkey::find_program_address(
         &[
             b"bin_array".as_ref(),
             pair.as_ref(),
@@ -19,15 +27,37 @@ pub fn get_bin_array_lower(
         ],
         program_id,
     );
-    (bin_array_lower_pubkey, bump)
+
+    let (bin_array_upper_pubkey, _) = Pubkey::find_program_address(
+        &[
+            b"bin_array".as_ref(),
+            pair.as_ref(),
+            (bin_array_index + 1).to_le_bytes().as_ref(),
+        ],
+        program_id,
+    );
+    (
+        bin_array_lower_pubkey,
+        bin_array_middle_pubkey,
+        bin_array_upper_pubkey,
+    )
 }
 
-pub fn get_bin_array_upper(
+pub fn get_pair_bin_array(
     bin_array_index: u32,
     pair: &Pubkey,
     program_id: &Pubkey,
-) -> (Pubkey, u8) {
-    let (bin_array_upper_pubkey, bump) = Pubkey::find_program_address(
+) -> (Pubkey, Pubkey) {
+    let (bin_array_lower, _) = Pubkey::find_program_address(
+        &[
+            b"bin_array".as_ref(),
+            pair.as_ref(),
+            (bin_array_index).to_le_bytes().as_ref(),
+        ],
+        program_id,
+    );
+
+    let (bin_array_upper, _) = Pubkey::find_program_address(
         &[
             b"bin_array".as_ref(),
             pair.as_ref(),
@@ -36,7 +66,42 @@ pub fn get_bin_array_upper(
         program_id,
     );
 
-    (bin_array_upper_pubkey, bump)
+    (bin_array_lower, bin_array_upper)
+}
+
+pub fn get_swap_hook_bin_array(bin_array_index: u32, hook: Pubkey) -> (Pubkey, Pubkey, Pubkey) {
+    let (hook_bin_array_lower, _) = Pubkey::find_program_address(
+        &[
+            b"bin_array".as_ref(),
+            hook.as_ref(),
+            (bin_array_index - 1).to_le_bytes().as_ref(),
+        ],
+        &rewarder_hook::ID,
+    );
+
+    let (hook_bin_array_middle, _) = Pubkey::find_program_address(
+        &[
+            b"bin_array".as_ref(),
+            hook.as_ref(),
+            (bin_array_index).to_le_bytes().as_ref(),
+        ],
+        &rewarder_hook::ID,
+    );
+
+    let (hook_bin_array_upper, _) = Pubkey::find_program_address(
+        &[
+            b"bin_array".as_ref(),
+            hook.as_ref(),
+            (bin_array_index + 1).to_le_bytes().as_ref(),
+        ],
+        &rewarder_hook::ID,
+    );
+
+    (
+        hook_bin_array_lower,
+        hook_bin_array_middle,
+        hook_bin_array_upper,
+    )
 }
 
 pub fn get_hook_bin_array(bin_array_index: u32, hook: Pubkey) -> (Pubkey, Pubkey) {
@@ -82,8 +147,15 @@ pub fn find_hook_position(lb_position: Pubkey, hook: Pubkey) -> Pubkey {
 
 pub fn find_bin_array_at_position(position: Position) -> (u32, [Pubkey; 2]) {
     let index = position.lower_bin_id / BIN_ARRAY_SIZE;
-    let (bin_array_lower, _) = get_bin_array_lower(index, &position.pair, &liquidity_book::ID);
-    let (bin_array_upper, _) = get_bin_array_upper(index, &position.pair, &liquidity_book::ID);
+
+    let (bin_array_lower, bin_array_upper) =
+        get_pair_bin_array(index, &position.pair, &liquidity_book::ID);
 
     (index, [bin_array_lower, bin_array_upper])
+}
+
+pub fn find_hook_bin_array_at_position(position_index: u32, hook: Pubkey) -> (u32, [Pubkey; 2]) {
+    let (hook_bin_array_lower, hook_bin_array_upper) = get_hook_bin_array(position_index, hook);
+
+    (position_index, [hook_bin_array_lower, hook_bin_array_upper])
 }
